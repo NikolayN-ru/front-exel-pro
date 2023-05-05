9870b4029b7b70497ded693803604472611096e7
Nikolay <67255397+NikolayN-ru@users.noreply.github.com>
2023-05-05 22:15:44 +0400

    03_0603_0603_06

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,68 +1,50 @@
-// // утилита взаимодействия с дом
-// // автоматизирование создание элемента и задавание ему класса
-// class Dom {}
-
-// export function $() {
-//   return new Dom();
-// }
-
-// $.create = (tagName, classes = "") => {
-//   const el = document.createElement(tagName);
-//   if (classes) {
-//     el.classList.add(classes);
-//   }
-//   return el;
-// };
-
 class Dom {
-    constructor(selector) {
-      this.$el = typeof selector === 'string'
+  constructor(selector) {
+    this.$el =
+      typeof selector === "string"
         ? document.querySelector(selector)
-        : selector
-    }
-  
-    html(html) {
-      if (typeof html === 'string') {
-        this.$el.innerHTML = html
-        return this
-      }
-      return this.$el.outerHTML.trim()
-    }
-  
-    clear() {
-      this.html('')
-      return this
-    }
-  
-    on() {
-  
-    }
-  
-    append(node) {
-      if (node instanceof Dom) {
-        node = node.$el
-      }
-  
-      if (Element.prototype.append) {
-        this.$el.append(node)
-      } else {
-        this.$el.appendChild(node)
-      }
-  
-      return this
-    }
+        : selector;
   }
-  
-  // event.target
-  export function $(selector) {
-    return new Dom(selector)
-  }
-  
-  $.create = (tagName, classes = '') => {
-    const el = document.createElement(tagName)
-    if (classes) {
-      el.classList.add(classes)
+
+  html(html) {
+    if (typeof html === "string") {
+      this.$el.innerHTML = html;
+      return this;
     }
-    return $(el)
+    return this.$el.outerHTML.trim();
   }
-  
+
+  clear() {
+    this.html("");
+    return this;
+  }
+
+  on() {}
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el;
+    }
+
+    if (Element.prototype.append) {
+      this.$el.append(node);
+    } else {
+      this.$el.appendChild(node);
+    }
+
+    return this;
+  }
+}
+
+// event.target
+export function $(selector) {
+  return new Dom(selector);
+}
+
+$.create = (tagName, classes = "") => {
+  const el = document.createElement(tagName);
+  if (classes) {
+    el.classList.add(classes);
+  }
+  return $(el);
+};
